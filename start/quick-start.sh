0ce34bae8f7c0ad56de5e09d1302faa6fe4601b0
Nathan Bleuzen <9270097+nathanbv@users.noreply.github.com>
2018-03-26 16:37:31 +0200

    Update quick-start.sh
    Changed path for config miracle file;
    Added some echo at each step;
    Fixed platform test.

--- start/quick-start.sh
+++ start/quick-start.sh
@@ -3,36 +3,42 @@
 ## First of, get the project source:
 # $ git clone https://github.com/nathanbv/miraclecast
 # $ cd miraclecast/start/
+# $ ./quick-start.sh
 
 ## Install dependencies:
+echo -e "\n>> Installing dependencies..."
 sudo apt update
 sudo apt install gstreamer1.0-plugins-bad gstreamer1.0-plugins-base gstreamer1.0-plugins-good gstreamer1.0-plugins-ugly gstreamer1.0-pulseaudio gstreamer1.0-alsa gstreamer1.0-tools gstreamer1.0-libav libglib2.0-dev autoconf autogen libtool libudev-dev libsystemd-dev libreadline-dev -y
 
+# The following dependencies are need only on RPi/ARM:
 PLTFM=$(cat /proc/cpuinfo | grep ARM)
-if [ -ez "$PLTFM" ];
+if [ "$PLTFM" ];
 then
-  # The following dependencies are need only on RPi/ARM:
   sudo apt install gstreamer1.0-omx gstreamer1.0-omx-generic gstreamer1.0-rtsp -y
 fi
 
 # Checks
 cd ../res
-sudo ./test-viewer.sh
+echo -e "\n>> Hardware capabilitiy check:"
 sudo ./test-hardware-capabilities.sh
-read -p "Please verify results of above checks. Then press any key to continue... " -n1 -s
+echo -e "\n>> Software requirement check:"
+sudo ./test-viewer.sh
+echo ">> Please verify results of above checks."
+read -p "   Then press any key to continue... " -n1 -s
 
 ## Build MiracleCast project:
+echo -e "\n>> Start building MiracleCast..."
 cd ..
 mkdir build
 cd build
 ../autogen.sh g
 make
 sudo make install
-mkdir ~/.config/
-cp miraclecastrc ~/.config/
+cp ../start/miraclecastrc /root/.config/
 
 ## Run MiracleCast as sink:
 # Kill wpa_supplicant and NetworkManager to allow miracle-wifid to create a wifi-p2p interface
+echo -e "\n>> Launching MiracleCast for the first time..."
 cd ../res/
 ./launch.sh
 
